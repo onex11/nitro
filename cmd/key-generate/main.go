d5e2153cd2a7c4f4a0291b2acf36f8b85c5d74af
xiaohuo <xiaohuo200@gmail.com>
2023-11-29 14:47:15 +0800

    feat: key generation

---
+++ cmd/key-generate/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func main() {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		panic(err)
+	}
+
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	publicKey := privateKey.Public()
+	publicKeyECDSA, _ := publicKey.(*ecdsa.PublicKey)
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	fmt.Printf("address: %s, private key: %s\n", address, hexutil.Encode(privateKeyBytes)[2:])
+
+}
